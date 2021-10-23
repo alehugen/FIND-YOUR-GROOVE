9bf633736c5034aeb186c576aab600bc1c3a8d4b
Alessandro Hugen <alessandrohugen@gmail.com>
2021-10-22 22:14:13 -0300

    update readme

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import linkeDin  from '../images/linkedin_icon.png';
+import gitHub  from '../images/github_icon2.png';
 
 export default function Footer() {
   return(
     <div className="footer-box">
-      <p>Developer by</p>
-      <a href="https://www.linkedin.com/in/alehugen/" target='_blank' rel='noreferrer'>Alessandro Hugen</a>
+      <a href="https://www.linkedin.com/in/alehugen/" target="_blank" rel="noreferrer">
+      <img src={ linkeDin } alt="icone linkedin" />
+      </a>
+      <p>Developer by Alessandro Hugen</p>
+      <a href="https://github.com/alehugen" target="_blank" rel="noreferrer">
+      <img src={ gitHub } alt="icone github" className='gh' />
+      </a>
     </div>
   );
 }
